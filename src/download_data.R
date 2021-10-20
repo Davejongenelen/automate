ed7f7857047eccd2e61ea0882b65a7df91e8b511
Davejongenelen <d.c.r.jongenelen@tilburguniversity.edu>
2021-10-20 07:59:40 +0200

    e

--- src/download_data.R
+++ src/download_data.R
@@ -7,8 +7,10 @@ download_data <- function(url, filename){
   download.file(url = url, destfile = paste0(filename, ".csv"))
 }
 
+dir.create("data")
 url_listings <- "http://data.insideairbnb.com/belgium/vlg/antwerp/2021-02-25/visualisations/listings.csv"
 url_reviews <- "http://data.insideairbnb.com/belgium/vlg/antwerp/2021-02-25/visualisations/reviews.csv"
 
 download_data(url_listings, "data/listings")
 download_data(url_reviews, "data/reviews") 
+
